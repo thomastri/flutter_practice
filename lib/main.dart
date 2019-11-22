d080768a6295154320d20d20a3ee5f42ffc5a2b4
mrtortuga <thomas.tree.le@gmail.com>
2019-11-22 10:18:44 -0500

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
+    return MaterialApp(
+      title: 'Startup name generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+// Creates a Stateful widget (stateful means it can change over time)
+// Basically a new object that we can use
+// StatefulWidget itself is a stateless object
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+// The state itself is stateful. It can change. We need both methods for a new obj.
+// Build seems to be a basic returner
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // underscores enforce privacy in dart
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup name generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          // Adds border
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            // when we approach the end of the screen, load ten more
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
